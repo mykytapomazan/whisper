e5c385fb068469ebe7f28d759161dfce177515e3
Mykyta Pomazan <mykytapomazan@gmail.com>
2024-10-23 17:37:54 +0300

    Build and style side navbar, post form, post modal, and individual post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
 
-  validates :body, presence: true, length: { minimum: 3, maximum: 500 }
+  validates :body, presence: true, length: { minimum: 3, maximum: 280 }
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,5 +4,5 @@ RSpec.describe Post, type: :model do
   it { should belong_to(:user) }
   it { should validate_presence_of(:body) }
   it { should validate_length_of(:body).is_at_least(3) }
-  it { should validate_length_of(:body).is_at_most(500) }
+  it { should validate_length_of(:body).is_at_most(280) }
 end
